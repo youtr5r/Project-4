0152071e453e94b3d94b9e32135663f6d649850f
Tristan Young <tristan.young@verizon.com>
2019-07-09 15:23:56 -0500

    update

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,30 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+    createPhrases() {
+        let phrases = [
+            new Phrase('fail early and fail often'),
+            new Phrase('To be or not to be'),
+            new Phrase('Jacob I am your father'),
+            new Phrase('Excuses are tools of the incompetent'),
+            new Phrase('JavaScript is fun'),
+            new Phrase('Classes contain objects'),
+            new Phrase('Objects have properties'),
+            new Phrase('Encapsulation is data hidden inside an object'),
+            new Phrase('An array is a sequence of data')
+           
+        ];
+        return phrases;
+        //update this with phrases.length
+        function getRandomPhrase() {
+            return Math.floor(Math.random() * Math.floor
+        }
+    }
+ }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+const phrase = new Phrase('Life is like a box of chocolates');
+const game = new Game();
+
+game.phrases.forEach((phrase, index) => {
+console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
+
+//console.log(`Phrase - phrase: ${phrase.phrase}`);
