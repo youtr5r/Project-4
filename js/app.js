29360044fc7a9c3c042f557230fa84842338f355
Tristan Young <tristan.young@verizon.com>
2019-07-15 14:20:38 -0500

    update

--- js/app.js
+++ js/app.js
@@ -10,15 +10,4 @@ $('.key').on('click', function (key) {
     game.handleInteraction(key);
 });
 
-//adds keyboard functionality for game.
-$(document).on('keyup', function (e) {
-    let letterKey = [];
-    if (letterKey.includes(e.key.toLowerCase())) {
-        for (let key of  $('.key')) {
-            if (key.innerText == e.key.toLowerCase()) {
-                key.click();
-            }
-        }
-    }
-    
-});
+
